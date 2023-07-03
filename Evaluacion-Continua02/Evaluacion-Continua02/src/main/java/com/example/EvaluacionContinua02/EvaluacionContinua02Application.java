9431ed887ad5649a9ed8d424038bf5be2db66a76
francia paucar <134339070+valery-jazminfrancia@users.noreply.github.com>
2023-07-03 15:46:35 -0500

    Add files via upload

---
+++ Evaluacion-Continua02/Evaluacion-Continua02/src/main/java/com/example/EvaluacionContinua02/EvaluacionContinua02Application.java
@@ -0,0 +1,13 @@
+package com.example.EvaluacionContinua02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionContinua02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionContinua02Application.class, args);
+	}
+
+}
---
+++ Evaluacion-Continua02/Evaluacion-Continua02/src/test/java/com/example/EvaluacionContinua02/EvaluacionContinua02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EvaluacionContinua02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionContinua02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
